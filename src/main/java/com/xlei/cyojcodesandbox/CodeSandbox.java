69111bffeb84a666fc110344816beacae5bf53d8
mengliang <13767029108@163.com>
2023-09-21 09:57:55 +0800

    代码沙箱接口定义

---
+++ src/main/java/com/xlei/cyojcodesandbox/CodeSandbox.java
@@ -0,0 +1,17 @@
+package com.xlei.cyojcodesandbox;
+
+
+import com.xlei.cyojcodesandbox.model.ExecuteCodeRequest;
+import com.xlei.cyojcodesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+    /**
+     * 执行代码
+     * @param executeCodeRequest
+     * @return
+     */
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
