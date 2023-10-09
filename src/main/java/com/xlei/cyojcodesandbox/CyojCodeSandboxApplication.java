0a1184bce21622c9b7aa0ac71ebcc7c6f05983fe
mengliang <13767029108@163.com>
2023-10-09 19:28:56 +0800

    简单的计算示例

---
+++ src/main/java/com/xlei/cyojcodesandbox/CyojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.xlei.cyojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyojCodeSandboxApplication.class, args);
+    }
+
+}
