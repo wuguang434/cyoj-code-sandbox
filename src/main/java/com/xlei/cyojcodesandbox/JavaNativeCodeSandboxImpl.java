1304dc7dc0a5f9ffd2c33c6ed43684017923c2c3
mengliang <13767029108@163.com>
2023-10-10 18:54:07 +0800

    Java 原生代码沙箱实现(复用模板方法)

---
+++ src/main/java/com/xlei/cyojcodesandbox/JavaNativeCodeSandboxImpl.java
@@ -0,0 +1,16 @@
+package com.xlei.cyojcodesandbox;
+
+import com.xlei.cyojcodesandbox.model.ExecuteCodeRequest;
+import com.xlei.cyojcodesandbox.model.ExecuteCodeResponse;
+
+/**
+ * Java 原生代码沙箱实现(复用模板方法)
+ */
+public class JavaNativeCodeSandboxImpl extends JavaCodeSandboxTemplate {
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        return super.executeCode(executeCodeRequest);
+    }
+
+
+}
