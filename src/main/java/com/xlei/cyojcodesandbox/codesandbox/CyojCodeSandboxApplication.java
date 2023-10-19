a4bbae461b6532c6b299d8df510da92920dfda72
mengliang <13767029108@163.com>
2023-10-19 22:27:54 +0800

    启动类

--- src/main/java/com/xlei/cyojcodesandbox/codesandbox/CyojCodeSandboxApplication.java
+++ src/main/java/com/xlei/cyojcodesandbox/codesandbox/CyojCodeSandboxApplication.java
@@ -1,13 +1,16 @@
-package com.xlei.cyojcodesandbox;
-
+package com.xlei.cyojcodesandbox.codesandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 public class CyojCodeSandboxApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CyojCodeSandboxApplication.class, args);
+        System.out.println("====================代码沙箱启动成功====================");
     }
 
 }
